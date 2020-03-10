15da0198ed46f08d8402eb4b7afe2160355b07a0
ibicdlcod <504161487@qq.com>
2020-03-10 20:58:25 +0800

    server socket
    Signed-off-by: ibicdlcod <504161487@qq.com>

---
+++ readme/venv_init.md
@@ -0,0 +1,20 @@
+### linux install virtual environment:
+
+`apt-get install python3-venv`
+
+`python3 -m venv posix_venv`
+
+### linux use virtual environment:
+
+`source posix_venv/bin/activate`
+
+deactivate: type`deactivate`in shell
+
+### windows use virtual environment:
+#### cmd.exe
+
+`C:\> venv\Scripts\activate.bat`
+
+#### PowerShell
+
+`PS C:\> venv\Scripts\Activate.ps1`
--- src_server/main.py
+++ src_server/main.py
@@ -1,5 +1,20 @@
 from src_common import read_config
 import os
+import socket
 
-dict1 = read_config.read_primary_config(os.path.join("config", "config.ini"), "network")
-print(dict1)
+config_dict = read_config.read_primary_config(os.path.join("config", "config.ini"), "network")
+DEFAULT_HOST = config_dict["default_host"] if "default_server_listen" in config_dict.keys() else socket.gethostname()
+DEFAULT_PORT = int(config_dict["default_port"]) if "default_port" in config_dict.keys() else 1826
+print(DEFAULT_HOST, DEFAULT_PORT)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((DEFAULT_HOST, DEFAULT_PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
