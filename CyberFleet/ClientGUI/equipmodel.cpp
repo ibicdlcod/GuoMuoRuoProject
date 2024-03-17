7d34c6cd15bb1b4616a884e15061c727e8fb526f
ibicdlcod <504161487@qq.com>
2024-03-17 15:18:12 +0800

    mysterious bug
    Signed-off-by: ibicdlcod <504161487@qq.com>

--- CyberFleet/ClientGUI/equipmodel.cpp
+++ CyberFleet/ClientGUI/equipmodel.cpp
@@ -2,10 +2,13 @@
 #include <QJsonArray>
 #include <algorithm>
 #include "clientv2.h"
+#include "equipicon.h"
 
 #ifdef max
 #undef max
 #endif
+
+extern std::unique_ptr<QSettings> settings;
 
 EquipModel::EquipModel(QObject *parent, bool isInArsenal)
     : QAbstractTableModel{parent},
@@ -43,9 +46,6 @@ void EquipModel::setPageNumHint(int input) {
 
 void EquipModel::setRowsPerPageHint(int input) {
     int oldRowCount = rowCount();
-    QModelIndex topleft = this->index(0, 0);
-    QModelIndex bottomright = this->index(
-        std::max(rowsPerPage-1, input-1), numberOfColumns());
     rowsPerPage = input;
     int newRowCount = rowCount();
     if(oldRowCount < newRowCount) {
@@ -56,7 +56,7 @@ void EquipModel::setRowsPerPageHint(int input) {
     else if(oldRowCount > newRowCount) {
         removeRows(newRowCount, oldRowCount - newRowCount);
     }
-    emit dataChanged(topleft, bottomright, QList<int>());
+    wholeTableChanged();
 }
 
 void EquipModel::setIsInArsenal(bool input) {
@@ -65,10 +65,10 @@ void EquipModel::setIsInArsenal(bool input) {
 
 int EquipModel::numberOfColumns() const {
     if(isInArsenal) {
-        return 4; // equip/star/destruct/addstar
+        return 7; // uid/equip/star/attr/destruct/addstar/hiddensort
     }
     else
-        return 2; // equip/star
+        return 5; // uid/equip/star/attr/hiddensort
 }
 
 int EquipModel::numberOfEquip() const {
@@ -91,18 +91,122 @@ int EquipModel::columnCount(const QModelIndex &parent) const {
 }
 
 QVariant EquipModel::data(const QModelIndex &index, int role) const {
-    if(role != Qt::DisplayRole)
-        return QVariant();
+    int realRowIndex = index.row() + rowsPerPage * pageNum;
+    Q_ASSERT(sortedEquipIds.length() > realRowIndex);
+    QUuid uidToDisplay = sortedEquipIds[realRowIndex];
+    Equipment *equipToDisplay = clientEquips[uidToDisplay];
+    int starToDisplay = clientEquipStars[uidToDisplay];
+
     Clientv2 &engine = Clientv2::getInstance();
-    if(engine.isEquipRegistryCacheGood()) {
-        return "FB";
+    bool ready = engine.isEquipRegistryCacheGood();
+    if(!ready)
+        return QVariant();
+    switch (role) {
+    case Qt::DisplayRole: {
+        if(index.column() == uidCol) {
+            return uidToDisplay.toString();
+        }
+        else if(index.column() == equipCol) {
+            return equipToDisplay->toString(
+                settings->value("client/language", "ja_JP").toString());
+        }
+        else if(index.column() == hiddenSortColumn()) {
+            return QString::number(equipToDisplay->type.getTypeSort());
+        }
+        else {
+            return "FB";
+        }
     }
-    else {
-        return "FA";
+    break;
+    case Qt::DecorationRole: {
+        if(index.column() == equipCol) {
+            //return Icute::equipIcon(equipToDisplay->type, false);
+        }
+    }
+    break;
+    case Qt::EditRole:
+    case Qt::ToolTipRole:
+    case Qt::StatusTipRole:
+    case Qt::WhatsThisRole:
+    case Qt::SizeHintRole:
+    case Qt::FontRole:
+    case Qt::TextAlignmentRole:
+    case Qt::BackgroundRole:
+    case Qt::ForegroundRole:
+    case Qt::CheckStateRole:
+    case Qt::InitialSortOrderRole: {
+        if(index.column() == hiddenSortColumn())
+            return Qt::AscendingOrder;
+        else
+            return QVariant();
+    }
+    break;
+    case Qt::AccessibleTextRole:
+    case Qt::AccessibleDescriptionRole:
+    default: return QVariant(); break;
     }
 }
 
+QVariant EquipModel::headerData(int section, Qt::Orientation orientation,
+                            int role) const {
+    switch (role) {
+    case Qt::DisplayRole: {
+        if(orientation == Qt::Vertical) {
+            int realRowIndex = section + rowsPerPage * pageNum;
+            return QString::number(realRowIndex);
+        }
+        else {
+            if(section == uidCol)
+                return qtTrId("equip-uuid");
+            else if(section == equipCol)
+                return qtTrId("equip-name");
+            else if(section == starCol)
+                return qtTrId("equip-star");
+            else if(section == attrCol)
+                return qtTrId("equip-attr");
+            else if(section == destructColumn())
+                return qtTrId("destruct");
+            else if(section == addStarColumn())
+                return qtTrId("equip-improve");
+            else
+                return QVariant();
+        }
+    }
+    break;
+    case Qt::DecorationRole:
+    case Qt::EditRole:
+    case Qt::ToolTipRole:
+    case Qt::StatusTipRole:
+    case Qt::WhatsThisRole:
+    case Qt::SizeHintRole:
+    case Qt::FontRole:
+    case Qt::TextAlignmentRole:
+    case Qt::BackgroundRole:
+    case Qt::ForegroundRole:
+    case Qt::CheckStateRole:
+    case Qt::InitialSortOrderRole:
+    case Qt::AccessibleTextRole:
+    case Qt::AccessibleDescriptionRole:
+    default: return QVariant(); break;
+    }
+}
+
+int EquipModel::destructColumn() const {
+    return isInArsenal ? 4 : -1;
+}
+
+int EquipModel::addStarColumn() const {
+    return isInArsenal ? 5 : -1;
+}
+
+int EquipModel::hiddenSortColumn() const {
+    return isInArsenal ? 6 : 4;
+}
+
 void EquipModel::updateEquipmentList(const QJsonObject &input) {
+    clientEquips.clear();
+    clientEquipStars.clear();
+    sortedEquipIds.clear();
     int oldRowCount = rowCount();
     static QMetaObject::Connection connection;
     Clientv2 &engine = Clientv2::getInstance();
@@ -117,16 +221,31 @@ void EquipModel::updateEquipmentList(const QJsonObject &input) {
             int star = itemObject["star"].toInt();
             clientEquips[uid] = equip;
             clientEquipStars[uid] = star;
+            sortedEquipIds.append(uid);
         }
+        std::sort(sortedEquipIds.begin(),
+                  sortedEquipIds.end(),
+                  [this](QUuid a, QUuid b)
+                  {
+                      if((*clientEquips[a]).isNotEqual(*clientEquips[b]))
+                          return (*clientEquips[a]) < (*clientEquips[b]);
+                      else
+                          return a < b;
+                  });
         int newRowCount = rowCount();
-        beginInsertRows(QModelIndex(), 0,
-                        newRowCount - oldRowCount - 1);
-        endInsertRows();
+        if(oldRowCount < newRowCount) {
+            beginInsertRows(QModelIndex(), 0,
+                            newRowCount - oldRowCount - 1);
+            endInsertRows();
+        }
+        else if(oldRowCount > newRowCount) {
+            removeRows(newRowCount, oldRowCount - newRowCount);
+        }
         wholeTableChanged();
         emit needReCalculateRows();
     }
     else {  /*
-        connection = connect(&engine, &Clientv2::equipRegistryComplete,
+            connection = connect(&engine, &Clientv2::equipRegistryComplete,
                              this, [this, &input](){
             QTimer::singleShot(100, [=, this](){updateEquipmentList(input);});});
             */
